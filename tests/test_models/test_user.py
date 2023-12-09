c00d0e1d1a2ae70a6d0161fc3e98e9859400f848
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-12-09 16:57:30 +0200

    add tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,11 +6,26 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from models.user import User
 import unittest
 
 
 class Testuser(unittest.TestCase):
-    """
-    Unittests for the User class.
-    """
-    pass
+    """Unittest for testing the User class."""
+
+    def setUp(self):
+        self.model = User()
+        self.model_dict = self.model.to_dict()
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.model, User)
+
+    def test_attr_empty_string(self):
+        self.assertEqual(self.model.email, "")
+        self.assertEqual(self.model.password, "")
+        self.assertEqual(self.model.first_name, "")
+        self.assertEqual(self.model.last_name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
